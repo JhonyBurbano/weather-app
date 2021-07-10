345f34ab976299c964befdd301686ba52e60271d
JhonyBurbano <jhonyburbanojb@gmail.com>
2021-07-10 18:05:02 -0500

    Mapbox Search API y Token de acceso

--- models/searches.js
+++ models/searches.js
@@ -15,7 +15,7 @@ class Searches {
         try {
             // Peticion http
             // console.log('Ciudad - ', place);
-            const resp = await axios.get('https://reqres.in/api/users?page=2');
+            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/madri.json?access_token=pk.eyJ1IjoiamhvbnlidXJiYW5vIiwiYSI6ImNrcXlkMGt0dzE3N2Uyb25scGM1NzBxdXcifQ.LopYmaYelu7t-rO47RhGYw&limit=5&language=es');
             console.log(resp.data);
 
             return []; // retorna las ciudades encontrdas   
