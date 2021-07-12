63b1d766407b1f96d98cb1cb716a1c25d5985ba7
JhonyBurbano <jhonyburbanojb@gmail.com>
2021-07-12 09:40:39 -0500

    Clase Crear instancias de Axios

--- models/searches.js
+++ models/searches.js
@@ -7,6 +7,14 @@ class Searches {
         // TODO: Leer base de datos si existe
     }
 
+    get paramsMapbox() {
+        return {
+            'access_token': 'pk.eyJ1IjoiamhvbnlidXJiYW5vIiwiYSI6ImNrcXlkMGt0dzE3N2Uyb25scGM1NzBxdXcifQ.LopYmaYelu7t-rO47RhGYw',
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+
     /*
     ciudad = city
     lugar = place
@@ -14,8 +22,11 @@ class Searches {
     async city(place = '') {
         try {
             // Peticion http
-            // console.log('Ciudad - ', place);
-            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/madri.json?access_token=pk.eyJ1IjoiamhvbnlidXJiYW5vIiwiYSI6ImNrcXlkMGt0dzE3N2Uyb25scGM1NzBxdXcifQ.LopYmaYelu7t-rO47RhGYw&limit=5&language=es');
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json`,
+                params: this.paramsMapbox
+            })
+            const resp = await instance.get();
             console.log(resp.data);
 
             return []; // retorna las ciudades encontrdas   
